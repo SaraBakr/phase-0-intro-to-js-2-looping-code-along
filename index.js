a2ccbefdde4a23c3e873e9f94daa46fddb71c9a6
sara ahmed <sara.bakr.ahmad@gmail.com>
2022-11-20 00:48:24 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(arr, event) {
+    let message = []
+    for (let i = 0; i < arr.length; i++) {
+        message.push("Thank you, " + arr[i] + ", for the wonderful " + event + " gift!");
+    }
+    return message;
+};
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+countDown(10);
+countDown(4);
